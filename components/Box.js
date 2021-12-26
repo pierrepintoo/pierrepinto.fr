9aa0ccb9aa65ac08ea56f9dcace6047675842d43
Pierrot le pépin <pierrot@MacBook-Pro-de-Pierrot.local>
2021-12-27 00:28:37 +0100

    ✨ Fixed eslint config

--- components/Box.js
+++ components/Box.js
@@ -1,30 +1,38 @@
- 	
-
-import ReactDOM from 'react-dom'
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 
-const Box = (props) => {
+const Box = () => {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  /* eslint arrow-body-style: ["error", "always"] */
+  /* eslint-env es6 */
+  useFrame(() => {
+    /* eslint no-return-assign: "error" */
+    return (ref.current.rotation.x += 0.01)
+  })
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
-      {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onClick={() => {
+        return click(!clicked)
+      }}
+      onPointerOver={() => {
+        return hover(true)
+      }}
+      onPointerOut={() => {
+        return hover(false)
+      }}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 
-export default Box;
+export default Box
